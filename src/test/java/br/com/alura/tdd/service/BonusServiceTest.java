1455efb8847290b8de7bc6a45c13a44068654c63
F0655369 Amanda Queiroz Sena <amandasena97@gmail.com>
2023-04-03 19:19:21 -0300

    mais um metodo no teste

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -27,4 +27,12 @@ class BonusServiceTest {
         assertEquals(new BigDecimal("250.00"), bonus);
     }
 
+    @Test
+    void bonusDeveriaSer10PorCentoParaSalarioDeExatamenteDezMil() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus (new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+
 }
